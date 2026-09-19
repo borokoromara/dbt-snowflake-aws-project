3a8815da0ea57562b5f60195dfa2598a11624213
borokoromara <borokoromara@gmail.com>
2026-09-19 20:21:10 +0200

    Revise CSV file format and add data copy commands
    Updated file format and added COPY INTO statements for bookings, hosts, and listings.

--- DDL/resources.sql
+++ DDL/resources.sql
@@ -1,16 +1,21 @@
-CREATE OR REPLACE FILE FORMAT IF NOT EXISTS csv_format
-  TYPE = 'CSV' 
-  FIELD_DELIMITER = ','
-  SKIP_HEADER = 1
-  ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE;
+CREATE OR REPLACE FILE FORMAT airbnb_csv_format
+    TYPE = CSV
+    SKIP_HEADER = 1
+    FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+    EMPTY_FIELD_AS_NULL = TRUE;
 
 
-CREATE OR REPLACE STAGE snowstage
-FILE_FORMAT = csv_format
-URL='your_s3_bucket_path';
-    
+COPY INTO BOOKINGS
+FROM @my_s3_stage
+FILES = ('bookings.csv')
+FILE_FORMAT = airbnb_csv_format;
 
-COPY INTO <your_table_name>
-FRoM @snowstage
-FILES=('your_file_name.csv')
-CREDENTIALS=(aws_key_id = 'yourkey', aws_secret_key = 'yoursecretkey');
+COPY INTO HOSTS
+FROM @my_s3_stage
+FILES = ('hosts.csv')
+FILE_FORMAT = airbnb_csv_format;
+
+COPY INTO LISTINGS
+FROM @my_s3_stage
+FILES = ('listings.csv')
+FILE_FORMAT = airbnb_csv_format;
